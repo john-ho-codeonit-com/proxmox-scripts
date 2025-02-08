2394d7fa9a91f83cf313723347ef19115dea3047
John Ho <john.ho@codeonit.com>
2025-02-08 00:06:48 -0500

    WIP

---
+++ create-ct.sh
@@ -0,0 +1,200 @@
+#!/usr/bin/env bash
+
+check_script_running() {
+    pid=`pgrep -f $0`
+    if ! [[ -z "$pid" ]]; then
+        echo "❌ Script $0 already running with pid $pid.  Please close the running script to run."
+        exit 1
+    fi
+}
+
+
+# required
+hostname=
+
+# optional
+description=""
+vmid=
+memory=1024
+password="qwerty#123"
+gpu_passthrough=false
+enable_desktop=false
+enable_gpu_passthrough=false
+
+# non-configurable
+ct_ssh_public_keys="/home/john/.ssh/pve-ct_id_ed25519.pub"
+ostype="debian"
+rootfs="volume=vm:4"
+storage="vm"
+lxc_cgroup2_devices_allow_list="c 226:0 rwm|c 226:128 rwm|c 234:* rwm"
+lxc_mount_entry_list="/dev/dri dev/dri none bind,optional,create=dir|/dev/dri/renderD128 dev/renderD128 none bind,optional,create=file|/dev/kfd dev/kfd none bind,optional,create=file"
+cores=12
+cpulimit=12
+swap=0
+net0="name=eth0,firewall=1,ip=dhcp,bridge=vmbr0,type=veth"
+features="nesting=1"
+ssh_public_keys="/root/.ssh/authorized_keys"
+template="vm:vztmpl/debian-12-standard_12.7-1_amd64.tar.zst"
+
+usage() {
+  cat - >&2 <<EOF
+NAME
+    ${CMD:=${0##*/}} - Create a container
+ 
+SYNOPSIS
+    ${CMD:=${0##*/}} [-h|--help]
+                     --hostname=<arg>
+                     [--description=<arg>]
+                     [--vmid=<arg>]
+                     [--memory=<arg>]
+                     [--password=<arg>]
+                     [--enable-gpu-passthrough]
+                     [--enable-desktop]
+
+OPTIONS
+  -h, --help
+          Prints this and exits
+
+  --hostname=<arg>
+        hostname for container
+ 
+  --description=<arg>
+        description for container
+
+  --vmid=<arg>
+        vmid for container, will be the cluster nexid if not specified
+  
+  --memory=<arg>
+        memory for container in MB, will be 1024MB if not specified
+
+  --password=<arg>
+        password for container root user, will be qwerty#123 if not specified
+ 
+  --enable-gpu-passthrough
+        enables gpu passthrough if specified
+
+  --enable-desktop
+        add enable desktop and xrdp
+  
+EOF
+}
+
+fatal() {
+    for i; do
+        echo -e "${i}" >&2
+    done
+    exit 1
+}
+
+# For long option processing
+next_arg() {
+    if [[ $OPTARG == *=* ]]
+    then
+        # for cases like '--opt=arg'
+        OPTARG="${OPTARG#*=}"
+    else
+        # for cases like '--opt arg'
+        OPTARG="${args[$OPTIND]}"
+        OPTIND=$((OPTIND + 1))
+    fi
+}
+
+# ':' means preceding option character expects one argument, except
+# first ':' which make getopts run in silent mode. We handle errors with
+# wildcard case catch. Long options are considered as the '-' character
+optspec=":hfb:-:"
+args=("" "$@")  # dummy first element so $1 and $args[1] are aligned
+while getopts "$optspec" optchar; do
+    case "$optchar" in
+        h) usage; exit 0 ;;
+        enable-desktop) enable_desktop=true ;;
+        -) # long option processing
+            case "$OPTARG" in
+                help) usage; exit 0 ;;
+                enable-desktop) enable_desktop=true ;;
+                enable-gpu-passthrough) enable_gpu_passthrough=true ;;
+                hostname|hostname=*) next_arg
+                    hostname="$OPTARG" ;;
+                description|description=*) next_arg
+                    description="$OPTARG" ;;
+                vmid|vmid=*) next_arg
+                    vmid="$OPTARG" ;;
+                memory|memory=*) next_arg
+                    memory="$OPTARG" ;;
+                password|password=*) next_arg
+                    password="$OPTARG" ;;
+                -) break ;;
+                *) fatal "Unknown option '--${OPTARG}'" "see '${0} --help' for usage" ;;
+            esac
+            ;;
+        *) fatal "Unknown option: '-${OPTARG}'" "See '${0} --help' for usage" ;;
+    esac
+done
+
+fatal() {
+    for i; do
+        echo -e "${i}" >&2
+    done
+    exit 1
+}
+
+shift $((OPTIND-1))
+
+check_script_running
+
+if [ -z "${hostname}" ]; then
+    fatal "hostname is required"
+fi
+
+if [ -z "${vmid}" ]; then
+    vmid=$(pvesh get /cluster/nextid)
+fi
+
+
+
+pct create $vmid $template \
+  --hostname $hostname \
+  --description $description \
+  --cores $cores \
+  --cpulimit $cpulimit \
+  --memory $memory \
+  --swap $swap \
+  --features $features \
+  --net0 $net0 \
+  --ostype $ostype \
+  --password $password \
+  --rootfs $rootfs \
+  --storage $storage \
+  --unprivileged 1 \
+  --ssh-public-keys $ssh_public_keys \
+  --start 1
+
+# TODO: check to see if vmid exists
+sleep 5
+
+until [[ $(pct status $vmid | awk '{print $2}') == "running" ]]; do echo "waiting for container to start"; sleep 1; done
+
+ssh-keygen -f ~/.ssh/known_hosts -R $hostname
+
+cat $ct_ssh_public_keys | ssh root@$hostname -oStrictHostKeyChecking=accept-new 'cat >> /root/.ssh/authorized_keys'
+
+sed -i 's/#\?\(PermitRootLogin\s*\).*$/\1 without-password/' /etc/ssh/sshd_config
+ssh root@$hostname service sshd restart
+
+# TODO:  run other script
+curl -s <setup ct github script> | ssh root@$hostname 'bash -s -- --user_password=$password --enable-desktop=$enable_desktop'
+
+if ! [ -z "${enable_gpu_passthrough}" ]; then
+    apt install radeontop -y
+    pct shutdown $vmid
+    until [[ $(pct status $vmid | awk '{print $2}') == "stopped" ]]; do echo "waiting for container to start"; sleep 1; done
+    IFS='|' read -a lxc_cgroup2_devices_allow_list_array <<< "$lxc_cgroup2_devices_allow_list" 
+    for lxc_cgroup2_devices_allow in "${lxc_cgroup2_devices_allow_list_array[@]}"; do 
+        echo "$lxc_cgroup2_devices_allow" >> /etc/pve/lxc/$vmid
+    done
+    IFS='|' read -a lxc_mount_entry_list_array <<< "$lxc_mount_entry_list" 
+    for lxc_mount_entry in "${lxc_mount_entry_list_array[@]}"; do 
+        echo "$lxc_mount_entry" >> /etc/pve/lxc/$vmid
+    done
+    pct start $vmid
+fi
---
+++ setup-ct.sh
@@ -0,0 +1,149 @@
+#!/usr/bin/env bash
+
+check_script_running() {
+    pid=`pgrep -f $0`
+    if ! [[ -z "$pid" ]]; then
+        echo "❌ Script $0 already running with pid $pid.  Please close the running script to run."
+        exit 1
+    fi
+}
+
+# defaults
+user="dockeruser"
+user_fullname="Docker User"
+user_password="qwerty#123"
+enable_desktop=false
+
+usage() {
+  cat - >&2 <<EOF
+NAME
+    ${CMD:=${0##*/}} - Checks local environment to run the app
+ 
+SYNOPSIS
+    ${CMD:=${0##*/}} [-h|--help]
+                     [--user=<arg>]
+                     [--user-fullname=<arg>]
+                     [--user-password=<arg>]
+                     [--enable-desktop]
+
+OPTIONS
+  -h, --help
+          Prints this and exits
+
+  --user=<arg>
+        container user to created
+
+  --user_fullname=<arg>
+        user full name for the container user
+
+  --user_password=<arg>
+        user password for the container user
+
+  --enable-desktop
+        add enable desktop and xrdp
+
+EOF
+}
+
+fatal() {
+    for i; do
+        echo -e "${i}" >&2
+    done
+    exit 1
+}
+
+# For long option processing
+next_arg() {
+    if [[ $OPTARG == *=* ]]
+    then
+        # for cases like '--opt=arg'
+        OPTARG="${OPTARG#*=}"
+    else
+        # for cases like '--opt arg'
+        OPTARG="${args[$OPTIND]}"
+        OPTIND=$((OPTIND + 1))
+    fi
+}
+
+# ':' means preceding option character expects one argument, except
+# first ':' which make getopts run in silent mode. We handle errors with
+# wildcard case catch. Long options are considered as the '-' character
+optspec=":hfb:-:"
+args=("" "$@")  # dummy first element so $1 and $args[1] are aligned
+while getopts "$optspec" optchar; do
+    case "$optchar" in
+        h) usage; exit 0 ;;
+        enable-desktop) enable_desktop=true ;;
+        -) # long option processing
+            case "$OPTARG" in
+                help) usage; exit 0 ;;
+                enable-desktop) enable_desktop=true ;;
+                user|user=*) next_arg
+                    user="$OPTARG" ;;
+                user-fullname|user-fullname=*) next_arg
+                    user_fullname="$OPTARG" ;;
+                user-password|user-password=*) next_arg
+                    user_password="$OPTARG" ;;
+                -) break ;;
+                *) fatal "Unknown option '--${OPTARG}'" "see '${0} --help' for usage" ;;
+            esac
+            ;;
+        *) fatal "Unknown option: '-${OPTARG}'" "See '${0} --help' for usage" ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+check_script_running
+
+
+echo "user: $user"
+echo "user_fullname: $user_fullname"
+echo "user_password: $user_password"
+echo "enable_desktop: $enable_desktop"
+
+# update
+apt update
+apt upgrade -y
+
+# install docker
+apt-get install ca-certificates curl -y
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+# install dockage
+mkdir -p /opt/stacks /opt/dockge
+(cd /opt/dockge && # Download the compose.yaml
+curl https://raw.githubusercontent.com/louislam/dockge/master/compose.yaml --output compose.yaml && docker compose up -d)
+
+apt install sudo -y
+adduser $user --gecos "$user_fullname,,," --disabled-password
+echo "$user:$user_password" | chpasswd
+usermod -aG sudo $users
+usermod -aG docker $user
+
+if [[ "$enable_desktop" == true ]]; then
+    apt install xfce4 xfce4-goodies xorg dbus-x11 x11-xserver-utils -y
+    apt install xrdp -y
+    adduser xrdp ssl-cert
+    systemctl restart xrdp
+    usermod -aG video $user
+    usermod -aG render $user
+    usermod -aG audio $user
+    usermod -aG input $user
+fi
+
+mkdir -p /home/$user/.ssh
+cp /root/.ssh/authorized_keys /home/$user/.ssh
+chown -R john:john /home/$user/.ssh
+
+apt install git -y
