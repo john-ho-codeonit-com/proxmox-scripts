7c91a55bd23e7e9bf7e2133e24f392c64e2ad83d
John Ho <john.ho@codeonit.com>
2025-02-14 11:34:47 -0500

    WIP

--- create-ct.sh
+++ create-ct.sh
@@ -184,6 +184,8 @@ until [ -f "/etc/pve/lxc/$vmid.conf" ]; do echo "waiting for container to be cre
 until [ $(pct status $vmid | awk '{print $2}') == "running" ]; do echo "waiting for container to start..."; sleep 1; done
 until [ $(ssh-keyscan $hostname >/dev/null 2>&1)$? -eq 0 ]; do echo "waiting for container to start..."; sleep 1; done
 
+echo "xxxxxx"
+
 echo "Setting up ssh keys..."
 ssh-keygen -f ~/.ssh/known_hosts -R $hostname
 
