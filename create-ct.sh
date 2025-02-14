e43ab7eda61a0326a202ec89e1b7eb860a9beaaa
John Ho <john.ho@codeonit.com>
2025-02-14 12:36:15 -0500

    WIP

--- create-ct.sh
+++ create-ct.sh
@@ -8,8 +8,6 @@
 #     fi
 # }
 
-set -x
-
 # required
 hostname=
 
@@ -220,12 +218,5 @@ if [ -n "${docker_compose_url}" ]; then
     setup_ct_args+=" --docker-compose-url='$docker_compose_url'"
 fi
 
-# curl https://raw.githubusercontent.com/john-ho-codeonit-com/proxmox-scripts/refs/heads/main/setup-ct.sh?$(date +%s) --output /tmp/setup-ct.sh
-# chmod +x /tmp/setup-ct.sh
-#  ssh root@$hostname 'bash -s --' < /tmp/setup-ct.sh $setup_ct_args
-
-# (curl -s https://raw.githubusercontent.com/john-ho-codeonit-com/proxmox-scripts/refs/heads/main/setup-ct.sh?$(date +%s)| ssh root@$hostname bash -s -- $setup_ct_args) &
-# wait -n
-
 curl -s https://raw.githubusercontent.com/john-ho-codeonit-com/proxmox-scripts/refs/heads/main/setup-ct.sh?$(date +%s)| ssh root@$hostname bash -s -- $setup_ct_args
-ssh root@$hostname service sshd restart &
+ssh root@$hostname service sshd restart
