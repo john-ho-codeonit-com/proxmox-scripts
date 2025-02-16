c93e2fcb687923879d700b174468e583946c7a35
John Ho <john.ho@codeonit.com>
2025-02-16 14:23:33 -0500

    WIP

--- templates/llm/post-install.sh
+++ templates/llm/post-install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-(cd /opt/stacks/default && until [ "$(docker inspect -f {{.State.Health.Status}} $(docker-compose ps -q ollama))" = "healthy" ]; do sleep 1; done)
+(cd /opt/stacks/default && until [ "$(docker inspect -f {{.State.Status}} $(docker compose ps -q ollama))" = "running" ]; do echo "waiting for container to start..."; sleep 1; done)
 docker exec ollama ollama pull deepseek-r1:14b
