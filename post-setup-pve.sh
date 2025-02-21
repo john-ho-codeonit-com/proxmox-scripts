a27a48fc8ceda2a006cd48c0835899c440e1af4b
John Ho <john.ho@codeonit.com>
2025-02-20 22:31:38 -0500

    WIP

--- post-setup-pve.sh
+++ post-setup-pve.sh
@@ -1,29 +1,53 @@
-ls -la
-echo "setting up stuff..."
+echo "Install packages..."
+apt-get install sudo -y
 
-# curl --create-dirs -O --output-dir /usr/local/sbin https://raw.githubusercontent.com/john-ho-codeonit-com/proxmox-scripts/refs/heads/main/pve-shutdown
-# chmod 777 /usr/local/sbin/pve-shutdown
-# echo "0 1 * * * root /usr/local/sbin/pve-shutdown" > /etc/cron.d/pve-shutdown
+echo "Setting up TFA for root..."
+ROOT_OATHKEYID=$(oathkeygen) && echo -e OATH key ID for root: $OATHKEYID && qrencode -t ANSIUTF8 -o - $(echo "otpauth://totp/PVE:root@"$(hostname --fqdn)"?secret=$OATHKEYID")
+pveum user modify root@pam --keys $ROOT_OATHKEYID
 
-# bash <(curl -s https://raw.githubusercontent.com/BassT23/Proxmox/master/install.sh)
-# echo "0 0 * * * root /usr/local/sbin/update -s" > /etc/cron.d/update
+echo "Creating user..."
+$user=john
+$user_password
+useradd -m -s /bin/zsh $user
+usermod -aG sudo $user
+echo $user:$user_password | chpasswd
+pveum acl modify / --roles PVEAdmin --users john@pam
 
-# service cron restart
+echo "Setting up TFA for user..."
+USER_OATHKEYID=$(oathkeygen) && echo -e OATH key ID for $user: $OATHKEYID && qrencode -t ANSIUTF8 -o - $(echo "otpauth://totp/PVE:$user@"$(hostname --fqdn)"?secret=$OATHKEYID")
+pveum user modify $user@pam --keys $USER_OATHKEYID
 
-# wipefs --force --all /dev/nvme0n1
+echo "Enforcing TFA..."
+sudo pveum realm modify pam --tfa type=oath
 
-# pool=storage
-# zpool create -f -o ashift=12 $pool /dev/nvme0n1
-# zfs set compression=lz4 $pool
-# zfs create $pool/isos
-# zfs create $pool/containers
-# zfs create $pool/vms
-# zfs create $pool/backups
+# TODO:  setup ACME let's encrypt
 
-# pvesm add dir ISOs --path /$pool/isos --content iso,vztmpl
-# pvesm add dir Containers --path /$pool/containers --content rootdir
-# pvesm add dir VMs --path /$pool/vms --content images
-# pvesm add dir Backups --path /$pool/backups --content backup,snippets
+echo  "Setting up pve-shutdown cron job..."
+curl --create-dirs -O --output-dir /usr/local/sbin https://raw.githubusercontent.com/john-ho-codeonit-com/proxmox-scripts/refs/heads/main/pve-shutdown
+chmod 777 /usr/local/sbin/pve-shutdown
+echo "0 1 * * * root /usr/local/sbin/pve-shutdown" > /etc/cron.d/pve-shutdown
 
-# pveam update
-# pveam download ISOs debian-12-standard_12.7-1_amd64.tar.zst
+echo  "Setting up update cron job..."
+bash <(curl -s https://raw.githubusercontent.com/BassT23/Proxmox/master/install.sh)
+echo "0 0 * * * root /usr/local/sbin/update -s" > /etc/cron.d/update
+service cron restart
+
+pool=storage
+echo  "Setting up zfs pool..."
+wipefs --force --all /dev/nvme0n1
+zpool create -f -o ashift=12 $pool /dev/nvme0n1
+zfs set compression=lz4 $pool
+
+echo "Creating zfs pool directories..."
+zfs create $pool/isos
+zfs create $pool/containers
+zfs create $pool/vms
+zfs create $pool/backups
+pvesm add dir ISOs --path /$pool/isos --content iso,vztmpl
+pvesm add dir Containers --path /$pool/containers --content rootdir
+pvesm add dir VMs --path /$pool/vms --content images
+pvesm add dir Backups --path /$pool/backups --content backup,snippets
+
+echo "Download container templates..."
+pveam update
+pveam download ISOs debian-12-standard_12.7-1_amd64.tar.zst
--- setup-pve.sh
+++ setup-pve.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
+# TODO:  make command line
+
 hostname=x79pve
 
+echo "Setting up ssh key"
 ssh-keygen -f ~/.ssh/known_hosts -R $hostname
 ssh_public_key="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMdIkri7L7jgb6jAf+FrcxiE59OrxuHTw7TvWnt/jGBw john@Johns-MBP"
 echo "$ssh_public_key" | sshpass -p 'Kazsuma1' ssh root@$hostname -oStrictHostKeyChecking=accept-new 'cat >> /root/.ssh/authorized_keys'
 sshpass -p 'Kazsuma1' ssh root@$hostname "cat /root/.ssh/authorized_keys"
 
+echo "Running Post PVE Install script..."
 ssh -t root@x79pve 'bash -c "$(wget -qLO - https://github.com/community-scripts/ProxmoxVE/raw/main/misc/post-pve-install.sh)"'
 sleep 20
 until [ $(ssh-keyscan $hostname >/dev/null 2>&1)$? -eq 0 ]; do echo "waiting for reboot to complete..."; sleep 1; done
 
-curl -s "https://raw.githubusercontent.com/john-ho-codeonit-com/proxmox-scripts/refs/heads/main/post-install-setup.sh" \
+echo "Running Post Setup PVE script..."
+curl -s "https://raw.githubusercontent.com/john-ho-codeonit-com/proxmox-scripts/refs/heads/main/post-setup-pve.sh" \
      | ssh root@$hostname bash -s
 
-echo run other stuff
+# create containers
