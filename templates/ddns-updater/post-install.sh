43b1d2dd6f34cfbe78c37c0053429ee7dd47bc0d
John Ho <john.ho@codeonit.com>
2025-03-01 11:18:41 -0500

    WIP

---
+++ templates/ddns-updater/post-install.sh
@@ -0,0 +1,77 @@
+# check_script_running() {
+#     pid=`pgrep -f $0`
+#     if ! [[ -z "$pid" ]]; then
+#         echo "❌ Script $0 already running with pid $pid.  Please close the running script to run."
+#         exit 1
+#     fi
+# }
+
+# required
+
+# optional
+
+# non-configurables
+
+usage() {
+  cat - >&2 <<EOF
+NAME
+    ${CMD:=${0##*/}} - Create a container
+ 
+SYNOPSIS
+    ${CMD:=${0##*/}} [-h|--help]
+
+OPTIONS
+  -h, --help
+          Prints this and exits
+EOF
+}
+
+fatal() {
+    for i; do
+        echo -e "${i}" >&2
+    done
+    exit 1
+}
+
+# For long option processing
+next_arg() {
+    if [[ $OPTARG == *=* ]]
+    then
+        # for cases like '--opt=arg'
+        OPTARG="${OPTARG#*=}"
+    else
+        # for cases like '--opt arg'
+        OPTARG="${args[$OPTIND]}"
+        OPTIND=$((OPTIND + 1))
+    fi
+}
+
+# ':' means preceding option character expects one argument, except
+# first ':' which make getopts run in silent mode. We handle errors with
+# wildcard case catch. Long options are considered as the '-' character
+optspec=":hfb:-:"
+args=("" "$@")  # dummy first element so $1 and $args[1] are aligned
+while getopts "$optspec" optchar; do
+    case "$optchar" in
+        h) usage; exit 0 ;;
+        -) # long option processing
+            case "$OPTARG" in
+                help) usage; exit 0 ;;
+                -) break ;;
+                *) fatal "Unknown option '--${OPTARG}'" "see '${0} --help' for usage" ;;
+            esac
+            ;;
+        *) fatal "Unknown option: '-${OPTARG}'" "See '${0} --help' for usage" ;;
+    esac
+done
+
+fatal() {
+    for i; do
+        echo -e "${i}" >&2
+    done
+    exit 1
+}
+
+shift $((OPTIND-1))
+
+# check_script_running
