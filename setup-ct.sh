17e5154ea799755023802acd6af24312e0058c2b
John Ho <john.ho@codeonit.com>
2025-03-05 23:40:11 -0500

    WIP

--- setup-ct.sh
+++ setup-ct.sh
@@ -187,9 +187,10 @@ if [ "$package_url" ]; then
     touch $docker_default_stack_path/.env
     if curl -sfILo/dev/null "$package_url/.env"; then
         echo ...$package_env...
-        echo $(printf "%s\n" "$package_env" | jq -r 'to_entries | map("\(.key)=\(.value)") | @sh')"
-        eval "export $(printf "%s\n" "$package_env" | jq -r 'to_entries | map("\(.key)=\(.value)") | @sh')"
-        (cd $docker_default_stack_path && curl "$package_url/.env" --output .env && envsubst < .env | tee .env)
+        curl "$package_url/.env" --output $docker_default_stack_path/.env
+        cat $docker_default_stack_path/.env
+        # eval "export $(printf "%s\n" "$package_env" | jq -r 'to_entries | map("\(.key)=\(.value)") | @sh')"
+        (cd $docker_default_stack_path && envsubst < .env | tee .env)
     fi
     (cd $docker_default_stack_path && curl "$package_url/compose.yaml" --output compose.yaml && docker compose --env-file .env up -d)
 fi
