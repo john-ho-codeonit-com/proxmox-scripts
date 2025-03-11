70247e0baa6bd5089be8ef713b779c0d1f2eb213
John Ho <john.ho@codeonit.com>
2025-03-10 22:41:34 -0400

    WIP

--- setup.sh
+++ setup.sh
@@ -14,7 +14,7 @@ curl -s https://raw.githubusercontent.com/john-ho-codeonit-com/proxmox-scripts/r
      --mac="90:2B:34:56:10:2F" \
      --memory=4096 \
      --ssh-public-key="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIACmYd5vnc3vUyt5gpj/jKe4MMCnCCrzIqAscv0xO0lG john@Johns-MBP" \
-     --package-env='{"NAMECHEAP_API_KEY":"NAMECHEAP_API_KEY","NAMECHEAP_USER":"NAMECHEAP_USER","DOCKGE_URL":"dockge:5001","AUTHENTIK_URL":"authentik:9000","DDNSUPDATER_URL":"ddnsupdater:8000","OPENWEBUI_URL":"openwebui:3000"}'
+     --package-env='{"NAMECHEAP_API_KEY":"NAMECHEAP_API_KEY","NAMECHEAP_USER":"NAMECHEAP_USER","DOCKGE_URL":"192.168.50.185:5001","AUTHENTIK_URL":"192.168.50.46:9000","DDNSUPDATER_URL":"192.168.50.198:8000","OPENWEBUI_URL":"192.168.50.128:3000",CODESERVER_URL":"192.168.50.109:8443"}'
 
 curl -s https://raw.githubusercontent.com/john-ho-codeonit-com/proxmox-scripts/refs/heads/main/create-ct.sh \
      | bash -s -- \
@@ -40,7 +40,7 @@ curl -s https://raw.githubusercontent.com/john-ho-codeonit-com/proxmox-scripts/r
      --mac="90:2B:34:56:10:5F" \
      --package-url="https://raw.githubusercontent.com/john-ho-codeonit-com/proxmox-scripts/refs/heads/main/templates/openwebui" \
      --memory=16000 \
-     --size=120 \
+     --size=220 \
      --ssh-public-key="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIACmYd5vnc3vUyt5gpj/jKe4MMCnCCrzIqAscv0xO0lG john@Johns-MBP"
 
 curl -s https://raw.githubusercontent.com/john-ho-codeonit-com/proxmox-scripts/refs/heads/main/create-ct.sh \
@@ -56,7 +56,6 @@ curl -s https://raw.githubusercontent.com/john-ho-codeonit-com/proxmox-scripts/r
      | bash -s -- \
      --hostname=openmanus \
      --mac="90:2B:34:56:10:7F" \
-     --package-url="https://raw.githubusercontent.com/john-ho-codeonit-com/proxmox-scripts/refs/heads/main/templates/openmanus" \
      --memory=8192 \
      --size=120 \
      --ssh-public-key="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIACmYd5vnc3vUyt5gpj/jKe4MMCnCCrzIqAscv0xO0lG john@Johns-MBP"
